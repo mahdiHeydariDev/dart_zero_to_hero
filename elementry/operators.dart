47a0d01b0d57c0217b3073647d5a11deaac1c6b6
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-09-11 21:45:28 +0330

    All Operators in dart

---
+++ elementry/operators.dart
@@ -0,0 +1,23 @@
+void main() {
+  //Arithmetic Operators
+  print(10 + 10);
+  print(20 - 10);
+  print(20 / 2);
+  print(50 % 5);
+  print(2 * 10);
+  //Logical Operators
+  print(true && true);
+  print(true && false);
+  print(false && false);
+  print(true || true);
+  print(true || false);
+  print(false || false);
+  print(!true && !false);
+  //Relational Operators
+  print(10 > 5);
+  print(10 >= 5);
+  print(10 < 5);
+  print(10 <= 5);
+  print(10 == 5);
+  print(10 != 5);
+}
