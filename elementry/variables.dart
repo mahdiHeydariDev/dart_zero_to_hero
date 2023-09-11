d389588df01edc9ff285c51a1c466b344bede09d
mahdi heydari <mahdi.heydari.developer@gmail.com>
2023-09-11 21:45:39 +0330

    Variables and Types in dart

---
+++ elementry/variables.dart
@@ -0,0 +1,29 @@
+void main() {
+  var name = 'mahdi';
+  name = 'mo mahdi';
+  print(name);
+  //text
+  String firstName = 'mahdi';
+  String userName = 'mahdi123';
+  //number
+  int age = 20;
+  int floor = -3;
+  double average = 19.5;
+  num myAge = 19;
+  num myAverage = 17.77;
+  //true false
+  bool isOnline = true;
+  bool isOfline = false;
+  //List
+  List students = ['ali', 'reza', 'mamad', 1];
+  List<String> allStudents = ['ali', 'reza', 'mamad'];
+  //Map
+  Map json = {
+    'key': 'value',
+    'key2': 2,
+  };
+  Map<String, int> studentsScores = {
+    'ali': 20,
+    'reza': 16,
+  };
+}
